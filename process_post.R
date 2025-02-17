e778426f69afad16df644581fa40d40812efa7a2
amirgolzan <amirgolzan@gmail.com>
2025-02-17 11:35:27 -0800

    process post updated for the desired post to add

--- process_post.R
+++ process_post.R
@@ -7,7 +7,7 @@ library(fs)  # For file utilities
 # --- Settings / Inputs ---
 
 # Path to your .Rmd file
-input_rmd <- "_src/2023-02-11-changing-rstudio-theme.Rmd"
+input_rmd <- "_src/2022-02-11-PythonIntro.Rmd"
 
 # Today’s date (for post filename and figure folder)
 today_str <- as.character(Sys.Date())  
