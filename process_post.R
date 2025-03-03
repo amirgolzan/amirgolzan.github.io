3923db128f94626df4cfebeae57d2cb645262a9e
amirgolzan <amirgolzan@gmail.com>
2025-03-03 12:30:26 -0800

    new post added

--- process_post.R
+++ process_post.R
@@ -7,7 +7,7 @@ library(fs)  # For file utilities
 # --- Settings / Inputs ---
 
 # Path to your .Rmd file
-input_rmd <- "_src/2023-06-15-Viewing-Data-in-Python.Rmd"
+input_rmd <- "_src/2023-06-21-Cleaning-Data-in-Python.Rmd"
 
 # Today’s date (for post filename and figure folder)
 today_str <- as.character(Sys.Date())  
